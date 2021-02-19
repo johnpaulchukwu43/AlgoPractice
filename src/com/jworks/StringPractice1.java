be8bc1ef2eeeda412f49b65142255a163f4e3944
johnpaul <johnpaulchukwu43@gmail.com>
2021-02-19 15:57:06 +0100

    string practice question1

---
+++ src/com/jworks/StringPractice1.java
@@ -0,0 +1,10 @@
+package com.jworks;
+
+/*
+        Question1
+    Implement an algorithm to determine if a string has all unique characters. What
+    if you cannot use additional data structures?
+ */
+
+public class StringPractice1 {
+}
