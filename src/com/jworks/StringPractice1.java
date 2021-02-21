f3f7e5f4e0dd13fcfe712cd69352795ed600f93c
johnpaul <johnpaulchukwu43@gmail.com>
2021-02-22 00:32:40 +0100

    Worked on StringPractice1 question

--- src/com/jworks/StringPractice1.java
+++ src/com/jworks/StringPractice1.java
@@ -7,4 +7,26 @@ package com.jworks;
  */
 
 public class StringPractice1 {
+
+    private static boolean isUniqueCharacterString(String input){
+
+        int inputLength = input.length();
+        //Assuming the characters are ASCII then there are 256 possible values
+        if( inputLength> 256){
+            return false;
+        }
+
+        boolean[] input_array = new boolean[256];
+
+        for (int i = 0; i < inputLength; i++) {
+
+            int charInput = input.charAt(i);
+            if(input_array[charInput]){
+                return false;
+            }
+
+            input_array[charInput] = true;
+        }
+        return true;
+    }
 }
