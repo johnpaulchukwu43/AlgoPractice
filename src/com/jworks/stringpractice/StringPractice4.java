85a87d14a4b093e54a25d05d045a65934ffde816
johnpaul <johnpaulchukwu43@gmail.com>
2021-02-23 23:11:46 +0100

    Added StringPractice4 question

---
+++ src/com/jworks/stringpractice/StringPractice4.java
@@ -0,0 +1,15 @@
+package com.jworks.stringpractice;
+
+public class StringPractice4 {
+
+    /*
+    * Question
+    *  Given two strings, write a method to decide if one is a permutation of the other
+    * */
+
+
+    public static boolean isPalindrome(String one, String two){
+
+        return false;
+    }
+}
