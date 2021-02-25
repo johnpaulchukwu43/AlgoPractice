ab3310ddbd7b0de83c78a0e099e553eeb30f0850
johnpaul <johnpaulchukwu43@gmail.com>
2021-02-25 23:47:29 +0100

    Added Solution for StringPractice7

--- src/com/jworks/stringpractice/StringPractice7.java
+++ src/com/jworks/stringpractice/StringPractice7.java
@@ -7,6 +7,48 @@ public class StringPractice7 {
     a2blc5a3. If the "compressed" string would not become smaller than the original
     string, your method should return the original string*/
 
+    public static String compressString(String value){
 
+        /*
+         * Approach Taken:
+         * 1. Go through the string value, and check repeats and keep a count of it.
+         * 2. For each unique character found , append the character and count to string
+         * 3. When done, if length of compressed string is less than the original length, then return compressed string else return original value
+         * */
+
+        if(value == null || value.length() == 0) {
+            return value;
+        }
+
+        int originalLength = value.length();
+
+        char[] charValueArray = value.toCharArray();
+
+        StringBuilder compressedValue = new StringBuilder();
+
+        char last = charValueArray[0];
+
+        int count =0;
+
+        for (char c : charValueArray) {
+
+            if (c == last) {
+                count++;
+            } else {
+                compressedValue.append(last);
+                compressedValue.append(count);
+                last = c;
+                count = 1;
+
+            }
+        }
+
+        compressedValue.append(last);
+        compressedValue.append(count);
+
+        String compressedValueString = compressedValue.toString();
+        int compressedLength = compressedValueString.length();
+        return compressedLength < originalLength ? compressedValueString : value;
+    }
 
 }
