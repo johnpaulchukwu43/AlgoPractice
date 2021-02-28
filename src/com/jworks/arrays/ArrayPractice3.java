621aac120b24e97c8cdaae7ea0ed23a1b5e509ac
johnpaul <johnpaulchukwu43@gmail.com>
2021-02-28 22:22:44 +0100

    Added Solution for ArrayPractice3

--- src/com/jworks/arrays/ArrayPractice3.java
+++ src/com/jworks/arrays/ArrayPractice3.java
@@ -1,5 +1,7 @@
 package com.jworks.arrays;
 
+import java.util.Arrays;
+
 public class ArrayPractice3 {
 
     /*
@@ -26,5 +28,46 @@ Explanation: After calling your function, the input array is modified to: [1,0,0
              else
                 set the position [totalElementToDiscard + currentPosition] = currentValue
     */
+
+
+
+    public static int[] duplicateZeros(int[] arr) {
+
+
+        int length = arr.length - 1;
+
+        int totalElementToDiscard = 0;
+
+        for (int i = 0; i < length - totalElementToDiscard; i++) {
+
+            if(arr[i] == 0){
+                /*  To handle edge case where last element after removing the ones to be discarded is zero.
+                    For this there would be no space to duplicate another zero despite the last element being zero.
+                 */
+                if (i == length - totalElementToDiscard) {
+                    // For this zero we just copy it without duplication.
+                    arr[length] = 0;
+                    length -= 1;
+                    break;
+                }
+
+                totalElementToDiscard++;
+            }
+        }
+
+        int lastPos = length - totalElementToDiscard;
+        for (int i = lastPos; i>=0 ; i--) {
+
+            if(arr[i] == 0){
+                arr[totalElementToDiscard + i] = 0;
+                totalElementToDiscard--;
+                arr[totalElementToDiscard + i] = 0;
+            }else{
+                arr[totalElementToDiscard + i] = arr[i];
+            }
+        }
+
+        return arr;
+    }
 
 }
