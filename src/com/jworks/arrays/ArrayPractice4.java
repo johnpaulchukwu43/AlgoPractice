d471bb84fedcc6038b99870f90e5d96d6986cccd
johnpaul <johnpaulchukwu43@gmail.com>
2021-03-11 06:49:31 +0100

    Added mostOptimalMergeApproach for ArrayPractice4

--- src/com/jworks/arrays/ArrayPractice4.java
+++ src/com/jworks/arrays/ArrayPractice4.java
@@ -10,10 +10,16 @@ public class ArrayPractice4 {
     The number of elements initialized in nums1 and nums2 are m and n respectively.
     You may assume that nums1 has a size equal to m + n such that it has enough space to hold additional elements from nums2.
 
-    Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+    Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3 [1,2,3,0,0,0]
     Output: [1,2,2,3,5,6]
     */
 
+    public static void main(String[] args) {
+        // write your code here
+
+        System.out.println(Arrays.toString(moreOptimalMergeApproach(new int[]{1, 2, 3, 0, 0, 0}, 3, new int[]{2, 5, 6}, 3)));
+
+    }
 
     public static int[] naiveApproachMerge(int[] nums1, int m, int[] nums2, int n) {
 
@@ -60,5 +66,39 @@ public class ArrayPractice4 {
             }
         }
         return nums1;
+    }
+
+    public static int[] mostOptimalMergeApproach(int[] nums1, int m, int[] nums2, int n){
+         /*
+          Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6]
+            Approach taken:
+            1.  Create a read cursor R1, and point to m -1
+            2.  Create a read cursor R2, and point to n-1
+            3.  Create a write pointer W1 and point to end of m+n-1
+            4.  While W1 is still within nums1 length, check:
+                i. if nums2.length is greater than 0 and nums2[R2] > nums1[R1]:
+                    write nums1[W1] = nums2[R2]
+                   else:
+                     write nums1[W1] = nums2[R1]
+        */
+        int R1 = m-1;
+        int R2 = n-1;
+
+        for (int W1 = 0; W1 < m + n - 1; W1++) {
+
+            if(R2 < 0){
+                break;
+            }
+
+            if(R1 > 0 && nums1[R1] > nums2[R2]){
+                nums1[W1] = nums1[R1--];
+            }else{
+                nums1[W1] = nums2[R2--];
+            }
+        }
+
+        return nums1;
+
+
     }
 }
